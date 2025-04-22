11bd64ccb0b39635d506195f2cefa5aa6bf26e3e
mehdi283 <mehdisyed4321@gmail.com>
2025-04-22 18:23:33 +0400

    .

--- Chapter-6-Javascript/Exercises/indexx.js
+++ Chapter-6-Javascript/Exercises/indexx.js
@@ -1,41 +1,30 @@
 
 // Elements 
+const costPerLiterInput = document.getElementById('costPerLiter');
+const litersInput = document.getElementById('liters');
+const calculateBtn = document.getElementById('calculateBtn');
+const totalCostDisplay = document.getElementById('totalCost');
 
-const costPerLiterInput = document.getElementById('CostPerLiter');
 
-const litersInput = document.getElementById('Liters');
-
-const calculateBtn = document.getElementById('CalculateBtn');
-
-const totalCostDisplay = document.getElementById('TotalCost');
 
 
 
 // Click Button
-
 calculateBtn.addEventListener('click', () => {
 
 
+    
 
 // Convert values to numbers
-
 const costPerLiter = parseFloat(costPerLiterInput.value);
-
 const liters = parseFloat(litersInput.value);
 
 
 
-
-// This code will calculate total cost
-
+// Calculates the totalcost
 const totalCost = costPerLiter * liters;
 
 
-
-// This will display the results in two decimal numbers
-
-totalCostDisplay.textContent = `Total Cost: ${totalCost.toFixed(2)}`;
+// Display the result with 2 decimal places
+totalCostDisplay.textContent = `Total Cost: $${totalCost.toFixed(2)}`;
 });
-
-
-
